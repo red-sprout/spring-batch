f4f4184ab4c3de9cfb1c908ad82dfd6fccd890c9
red-sprout <jjw05015@gmail.com>
2026-04-14 01:40:04 +0900

    feat: FirstBatch 파일 작성 - job, step 설정

---
+++ src/main/java/org/example/springbatch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package org.example.springbatch.repository;
+
+import org.example.springbatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
